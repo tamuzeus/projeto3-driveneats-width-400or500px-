564b3403d85463546e5c7da3ca1146b3f97e99d4
Igor Tamuz <igortamuz@gmail.com>
2022-06-09 21:54:13 -0300

    master

---
+++ script.js
@@ -0,0 +1,9 @@
+function activeBorderFood(){
+    let painel = document.querySelector('.painel')
+    painel.classList.add('selectorFood')
+}
+
+function activeChecked(){
+    let buttonchecked = document.querySelector('.checked-button')
+    buttonchecked.classList.add('active')
+}
